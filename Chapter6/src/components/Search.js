129214fd7848780ef90fb9a5008a8ebace171117
mrliucan <mrliucan@foxmail.com>
2017-12-28 00:36:35 +1100

    Disable loading spinner on Search tab

--- Chapter6/src/components/Search.js
+++ Chapter6/src/components/Search.js
@@ -29,7 +29,11 @@ export default class Search extends Component {
             placeholderTextColor={globalStyles.MUTED_COLOR}
           />
         </View>
-        <NewsFeed news={this.props.filteredNews} listStyles={{}} />
+        <NewsFeed
+          news={this.props.filteredNews}
+          listStyles={{}}
+          showLoadingSpinner={false}
+        />
       </View>
     );
   }
