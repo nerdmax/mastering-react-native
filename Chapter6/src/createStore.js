65809186f8920198eee395d3668ae9aa6ae9cfc3
mrliucan <mrliucan@foxmail.com>
2017-12-28 00:14:16 +1100

    Add redux-promise middleware to fetch data from remote server

--- Chapter6/src/createStore.js
+++ Chapter6/src/createStore.js
@@ -1,19 +1,16 @@
-import {
-  createStore,
-  applyMiddleware,
-  combineReducers
-} from 'redux';
-import createLogger from 'redux-logger';
-import newsFeedReducer from './reducers/newsFeedReducer';
-import searchTermReducer from './reducers/searchTermReducer';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import createLogger from "redux-logger";
+import newsFeedReducer from "./reducers/newsFeedReducer";
+import searchTermReducer from "./reducers/searchTermReducer";
+import promiseMiddleware from "redux-promise";
+
 const logger = createLogger();
-export default (initialState = {}) => (
+export default (initialState = {}) =>
   createStore(
     combineReducers({
       news: newsFeedReducer,
-      searchTerm: searchTermReducer
+      searchTerm: searchTermReducer,
     }),
     initialState,
-    applyMiddleware(logger)
-  )
-);
+    applyMiddleware(logger, promiseMiddleware)
+  );
